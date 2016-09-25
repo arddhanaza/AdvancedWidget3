88241f836fdce7228c3c761bbe74ba04888cd877
Arddhana Zhafran <arddhanaza99@gmail.com>
2016-09-25 21:59:14 +0700

    Simple Data Binding Final

--- app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget3/MainActivity.java
@@ -1,8 +1,8 @@
 package id.sch.smktelkom_mlg.learn.advancedwidget3;
 
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import id.sch.smktelkom_mlg.learn.advancedwidget3.databinding.ActivityMainBinding;
 
@@ -14,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setNama(nama);
+    }
 }
